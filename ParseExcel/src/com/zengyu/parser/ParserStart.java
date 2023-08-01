1dff738fb759f22b7cee982600547ad93e8bdd8a
zhanzengyu <553838510@qq.com>
2023-08-01 16:21:08 +0800

    make project work in IDEA

--- ParseExcel/src/com/zengyu/parser/ParserStart.java
+++ ParseExcel/src/com/zengyu/parser/ParserStart.java
@@ -6,14 +6,19 @@ import java.util.List;
 public class ParserStart {
 
 	public static void main(String[] args) throws Exception {
+		String path = System.getProperty("user.dir");
+//		System.out.println(path);
+		String resourceDirPath = path + "/ParseExcel/resource/";
+		String inputFileName = resourceDirPath + "input.xls";
 		// 第一步：读取数据
-		List<StudentScore> dataList = new ExcelReader().read("resource/input.xls");
+		List<StudentScore> dataList = new ExcelReader().read(inputFileName);
 //		System.out.println(dataList);
 		// 第二步：排序
 		Collections.sort(dataList);
 //		System.out.println(dataList);
+		String outputFileName = resourceDirPath + "output.xls";
 		// 第三部：写入数据
-		new ExcelWriter().write("resource/output.xls", dataList);
+		new ExcelWriter().write(outputFileName, dataList);
 	}
 
 }
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # JavaSamples
-Samples for Java
+Java 实用例子集合
 
+### 1. Java Excel 读写
+代码仓库：[ParseExcel](https://github.com/nesger/JavaSamples/tree/master/ParseExcel)
 
-[ParseExcel](https://github.com/nesger/JavaSamples/tree/master/ParseExcel)
-
-Read and Write For Excel
+博客地址：[Java 读写 excel 实战完全解析](https://mp.weixin.qq.com/s/fXWpHiHR7mhVY1TJ2pAZQw)
